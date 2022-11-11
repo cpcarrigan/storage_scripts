d5da698b960ea1dba6605a3b91cd03523ecd0290
Cass Carrigan <cpcarrigan@gmail.com>
2022-11-10 18:18:11 -0800

    updating bulk hr delete script

--- hr_cleanup/hr_delete_bulk.py
+++ hr_cleanup/hr_delete_bulk.py
@@ -1,62 +1,104 @@
 #!/usr/bin/python3
 
+from datetime import datetime
 import re
 import requests
 import os
+import json
 import logging
 import argparse
 import sys
-sys.path.append("..") # Adds higher directory to python modules path.
+import glob
+import random
+
+sys.path.append("/home/ccarrigan/git/storage_scripts") # Adds higher directory to python modules path.
 from sf import utils2
 
 # Overview: Take the generated files, parse, and delete the objects:
 
 # sample file entries:
+# assetid,account,hr_url
 # 92536169,3677266552,http://s1.sf-cdn.com/v1/snapfish/migration_36516/hr_0004_233_285_0004233285546.jpg
 # 92536169,3677275633,http://s1.sf-cdn.com/v1/snapfish/migration_36516/hr_0004_233_295_0004233295234.jpg
 
 # todo:
-# handle the most generic format handed off by DBOPS - no tweaking ingest file
-# for thumbnail files: make call against TNL service, pull location, if you get valid URL, transform to storage, and then delete
-# deal w/ amazon images, cleversafe images
+
 # log anything that cannot be processed
 # log separately items to reprocess.
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("filename", help="Pass a filename to parse for URLs to delete")
-args = parser.parse_args()
-logging.basicConfig(filename=args.filename + '.log',level=logging.INFO,format='%(asctime)s.%(msecs)03d - %(levelname)s: %(message)s', datefmt='%d-%b-%y %H:%M:%S')
-logging.info("File to import: " + args.filename)
+STOP = '/home/ccarrigan/s1-deletion/work/STOP'
 
-files_object = utils2.ObjectStorage()
+while not os.path.exists(STOP):
+    try:
+        #     print(random.choice(os.listdir('work/ready')))
+        data_file = random.choice(os.listdir('work/ready'))
+        # sys.exit()
+        print(data_file)
+    except IndexError:
+        print("Exiting because no work files available.")
+        logging.critical("Exiting because no work files available.")
+        sys.exit()
+    except Exception as e:
+        print(f"Exiting. Failed to get a file to work on. Error occurred: {e}.")
+        logging.critical(f"Exiting. Failed to get a file to work on. Error occurred: {e}.")
+        sys.exit()
 
-f_pattern = re.compile(r"http://.*.sf-cdn.com/.*\.jpg$")
+    os.rename(f"work/ready/{data_file}", f"work/active/{data_file}")
+    dt_epoch = datetime.timestamp(datetime.now())
+    os.utime(f"work/active/{data_file}", (dt_epoch, dt_epoch))
 
-with open(args.filename, 'r') as f:
-  bulk_list = []
-  count = 10
-  status = False
+    s = requests.Session()
 
-  acct = ''
-  cluster = ''
-  for line in f:
-    ele = line.strip().split(',')
-    # logging.warning(f"looking at: '{line.rstrip()}'")
-    matched = f_pattern.match(ele[2])
-    if matched:
-      # use a regex to fix a problem, get two problems
-      url = ele[2]
-      logging.debug("Raw URL to delete: '" + url + "'")
-      url = files_object.clean_up_url(url)
-      logging.debug("Cleaned URL to delete: '" + url + "'")
+    logging.basicConfig(filename=f'work/log/{data_file}.log',level=logging.WARNING,format='%(asctime)s.%(msecs)03d - %(levelname)s: %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+    logging.info(f"File to import: work/active/{data_file}")
 
-      # need to add logic so that if the hostname or the account changes,
-      # submit the batch without waiting for the full set of data
-      if len(bulk_list) < count:
-        bulk_list.append(url)
-      else:
-        status = files_object.delete_openstack_batch(bulk_list)
-        logging.warning(f"Deletion status: {status} for bulk list of {len(bulk_list)} urls, beginning {bulk_list[0]}")
+    files_object = utils2.ObjectStorage()
+
+    # f_pattern = re.compile(r"http://([^.]+).sf-cdn.com/v1/([^/]+)/pg$")
+    f_pattern = re.compile(r"http://([^.]+).sf-cdn.com/v1/([^/]+)/([^/]+)/(.*)$")
+
+    with open(f"work/active/{data_file}", 'r') as f:
+    # with open(args.filename, 'r') as f:
         bulk_list = []
+        count = 1000
         status = False
+
+        acct = ''
+        cluster = ''
+        new_acct = False
+        for line in f:
+            ele = line.strip().split(',')
+            logging.info(f"looking at: '{line.rstrip()}'")
+            # 09-Nov-22 22:03:15.423 - WARNING: looking at: '2448034026,241339706026,http://s1.sf-cdn.com/v1/snapfish/migrationHosting_7426/hr_0273_943_350_0273943350026.jpg'
+            matched = f_pattern.match(ele[2])
+            if matched:
+                # use a regex to fix a problem, get two problems
+                url = ele[2]
+                logging.debug("Raw URL to delete: '" + url + "'")
+                url = files_object.clean_up_url(url)
+                logging.debug("Cleaned URL to delete: '" + url + "'")
+                if acct == '' and cluster == '':
+                    acct = matched.group(2)
+                    cluster = matched.group(1)
+                if acct != matched.group(2) or cluster != matched.group(1):
+                    new_acct = True
+
+                # need to add logic so that if the hostname or the account changes,
+                # submit the batch without waiting for the full set of data
+                if len(bulk_list) < count and new_acct == False:
+                    bulk_list.append(url)
+                else:
+                    status = files_object.delete_openstack_batch(bulk_list)
+                    logging.warning(f"Deletion status: {status} for bulk list of {len(bulk_list)} urls, beginning {bulk_list[0]}")
+                    bulk_list = []
+                    status = False
+
+    f.close()
+
+    os.rename(f"work/active/{data_file}", f"work/done/{data_file}")
+
+else:
+    print(f"Stop file ({STOP}) exists. Exiting")
+    log.critical(f"Stop file ({STOP}) exists. Exiting")
+    sys.exit()
