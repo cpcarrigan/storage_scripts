59c123d064f8a0d48eaaba54a3adf8bdc0193656
Cass Carrigan <cpcarrigan@gmail.com>
2022-12-09 13:37:24 -0800

    dedupe script changes

--- dedupe/a.py
+++ dedupe/a.py
@@ -5,6 +5,8 @@ import boto3
 import os
 import argparse
 import logging
+from dateutil.parser import parse as parsedate
+import time
 
 
 bucket = 'snapfish-prod-media-raw-snapfish'
@@ -12,7 +14,7 @@ bucket = 'snapfish-prod-media-raw-snapfish'
 parser = argparse.ArgumentParser()
 parser.add_argument("prefix", help=f"Pass a prefix/key to dedupe. Assumes the {bucket}")
 args = parser.parse_args()
-logging.basicConfig(filename='loggy.log',level=logging.WARN,format='%(asctime)s.%(msecs)03d - %(levelname)s: %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+logging.basicConfig(filename='a.log',level=logging.INFO,format='%(asctime)s.%(msecs)03d - %(levelname)s: %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 #logging.info("File to import: " + args.filename)
 prefix = args.prefix
 # prefix = '1f5eff/40758720050-1004851833784070.jpg'
@@ -28,38 +30,61 @@ client = boto3.client('s3')
 
 resp = client.list_object_versions(Prefix=prefix, Bucket=bucket)
 
-logging.warning(f"bucket: {bucket}")
-logging.warning(f"key: {prefix}\n")
+logging.warning(f"Bucket: {bucket}")
+logging.warning(f"Key: {prefix}\n")
 
 prev_version = ''
 prev_size = ''
-prev_date = ''
+# prev_date = ''
+#   url_datetime = parsedate(arr_line[2]).astimezone()
+#   now_datetime = datetime.datetime.fromtimestamp(time.time()).astimezone()
+prev_date = datetime.datetime.fromtimestamp(time.time()).astimezone()
+
+to_del = []
 
 for obj in [*resp['Versions'], *resp.get('DeleteMarkers', [])]:
     logging.warning(f"Key: {obj['Key']}")
     logging.warning(f"VersionId: {obj['VersionId']}")
     logging.warning(f"LastModified: {obj['LastModified']}")
     logging.warning(f"IsLatest: {obj['IsLatest']}")
-    logging.warning(f"Size: {obj['Size']}")
-    # logging.warning()
+    logging.warning(f"Size: {obj['Size']}\n")
 
     if prev_version == '':
-        logging.warning(f"Version unset, pulling in {obj['VersionId']}\n")
+        logging.info(f"No previous version stored, storing {obj['VersionId']}\n")
         prev_version = obj['VersionId']
         prev_size = int(obj['Size'])
-        # prev_date = obj['VersionId']
+        prev_date = obj['LastModified']
         next
     else:
         if prev_size > int(obj['Size']):
-            logging.warning(f"Old version: {prev_version} is greater than or equal to {obj['VersionId']}, keeping {prev_version}")
+            logging.warning(f"Saved version: {prev_version} is greater than or equal to {obj['VersionId']}, keeping {prev_version}")
+            to_del.append(obj['VersionId'])
+        # if sizes are equal, then keep whichever is newer:
         elif prev_size == int(obj['Size']): 
-            # elif prev_date >= obj['LastModified']
-            logging.warning('same size, keep newer')
+            logging.warning(f"Same size ({prev_size}B), comparing dates - {prev_date} vs {obj['LastModified']}")
+            # if stored date is newer, delete current evaluation
+            if prev_date >= obj['LastModified']:
+                logging.warning(f"Same size, saved version is newer or same date, keeping {prev_version}")
+                to_del.append(obj['VersionId'])
+            # else, delete the stored version, and save the current evaluation to stored version
+            else:
+                to_del.append(prev_version)
+                logging.warning(f"Same size, current evaluation is newer, keeping: {obj['VersionId']}")
+                prev_version = obj['VersionId']
+                prev_size = int(obj['Size'])
+                prev_date = obj['LastModified']
         else:
-            logging.warning(f"Replacementer is larger, {obj['Size']} vs {prev_size}, switching to version: {obj['Key']}")
+            logging.warning(f"Current evaluation is larger, {obj['Size']} vs {prev_size}, keeping current version: {obj['VersionId']}")
+            to_del.append(prev_version)
             prev_version = obj['VersionId']
             prev_size = int(obj['Size'])
+            prev_date = obj['LastModified']
             # logging.warning(f"{prev_version} is greater than or equal to {obj['VersionId']}, keeping {prev_version}")
+
+logging.warning(f"Will delete: {to_del}\n")
+for versionId in to_del:
+    logging.warning(f"Delete {versionId} of object {prefix} in bucket {bucket}")
+logging.warning(f"################################################")
 
 
 #for version in versions:
@@ -89,6 +114,6 @@ $ aws s3api list-object-versions --bucket snapfish-prod-media-raw-snapfish --pre
           "IsLatest": false,
           "LastModified": "2021-11-27T21:34:56.000Z"
        }
-                                                                                                                                                                                                                    ]
+    ]
  }
-                                                                                                                                                                                                                """
+"""
